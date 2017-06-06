50c58cf261fa3eea24edd284df164356263ccfb8
pabloferfer <pablo.f3rnand3z@gmail.com>
2017-06-06 19:49:24 +0200

    practica52

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -5,84 +5,77 @@ var paginate = require('../helpers/paginate').paginate;
 
 
 
-exports.randomPlay = function (req, res, next){
-    //función que comprueba pertenencia a un array
-    function contains(a, obj) {
-        for (var i = 0; i < a.length; i++) {
-            if (a[i] === obj) {
-                return true;
-            }
-        }
-        return false;
-    }
-    //cargo los ids y la puntuación
-    if (req.session.ids){
-        var ids = JSON.parse(req.session.ids);
-    }else{
-       var ids = [];
-    }
-    if (req.session.score){
-        var score = JSON.parse(req.session.score);
-    }else{
-        var score = 0;
-    }
-    //cargo las preguntas
+var contador =0;
+var hechas=[];
+
+
+// GET /quizzes/random_play
+exports.randomPlay = function (req, res, next) {
+
     models.Quiz.findAll()
-        .then(function(preguntas) {
-            if (score === preguntas.length) {
-                req.session.ids = JSON.stringify(0);
-                req.session.score = JSON.stringify(0);
-                res.render('quizzes/random_nomore', {
-                    score: score
-                });
-            }
-            else {
-                var n = Math.floor(Math.random() * preguntas.length);
-                while (contains(ids, n)) {
-                    n = Math.floor(Math.random() * preguntas.length);
+        .then(function (quizzes) {
+            if(quizzes.length > 0){
+                var random = parseInt(Math.random() * quizzes.length);
+                var quizID = quizzes[random].id;
+                var quiz = quizzes[random];
+                pregunta = quizID.question;
+                for (var i in hechas){
+                    if (hechas[i]===quizID){
+                        res.render('quizzes/random_play',{
+                            score: contador})
+                    } else if (hechas.length === quizzes.length) {
+                        res.render('quizzes/random_nomore', {score: contador})
+                    } else {
+                        hechas.push(quizID);
+                    }
                 }
-                var quiz = preguntas[n]
-                ids.push(n);
-                req.session.ids = JSON.stringify(ids);
-                req.session.score = JSON.stringify(score);
-                res.render('quizzes/random_play', {
-                    quiz: quiz,
-                    score: score
-                });
+                console.log(req.session.score);
+                console.log("Aqui va"+hechas);
+                res.render('quizzes/random_play',{
+                    score: contador,
+                    quiz: quiz
+                })
+            } else {
+                res.render('quizzes/random_nomore', {score: contador})
             }
-        });
-}
-
-//GET /quizzes/randomcheck
-exports.radomcheck = function (req, res, next) {
-
-    var answer = req.query.answer || "";
-    var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
-
-    var ids = JSON.parse(req.session.ids);
-    var score = JSON.parse(req.session.score);
-
-    if (!result){
-        req.session.ids = JSON.stringify([]);
-        req.session.score = JSON.stringify(0);
-        res.render('quizzes/random_result', {
-            score: score,
-            answer: answer,
-            result: result
-        });
-    }
-    else {
-        score++;
-        req.session.score = JSON.stringify(score);
-        req.session.ids = JSON.stringify(ids);
-        res.render('quizzes/random_result', {
-            score: score,
-            answer: answer,
-            result: result
-        });
-    }
+            return models.Quiz.findById(quizID);
+        })
 };
 
+exports.randomcheck = function (req, res, next) {
+    var answer = req.query.answer || "";
+
+    var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+    if(result){
+        contador += 1;
+        resu=contador;
+    }
+     else {
+        var resu=contador;
+        contador=0;
+    }
+    res.render('quizzes/random_result',{
+        score: resu,
+        result: result,
+        answer: answer});
+};
+
+// Autoload el quiz asociado a :quizId
+exports.load = function (req, res, next, quizId) {
+
+    models.Quiz.findById(quizId)
+    .then(function (quiz) {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('No existe ningún quiz con id=' + quizId);
+        }
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};
 
 // Autoload el quiz asociado a :quizId
 exports.load = function (req, res, next, quizId) {
